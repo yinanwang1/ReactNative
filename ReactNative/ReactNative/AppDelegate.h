ad82c30f2c34bec03e9c3b10959930854306e10b
ArthurWang <wangyn@qeebike.com>
2017-05-17 14:37:03 +0800

    Added a iOS project.

---
+++ ReactNative/ReactNative/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactNative
+//
+//  Created by ArthurWang on 2017/5/17.
+//  Copyright © 2017年 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
