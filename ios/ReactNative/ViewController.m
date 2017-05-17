d82415d5063a72643f5b5b3e18a6a27a94530c18
ArthurWang <wangyn@qeebike.com>
2017-05-17 20:46:39 +0800

    React Native processes is successful.

---
+++ ios/ReactNative/ViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  ReactNative
+//
+//  Created by ArthurWang on 2017/5/17.
+//  Copyright © 2017年 Arthur. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "RCTRootView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)onSelectedReactNatvieBtn:(id)sender
+{
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://10.0.55.146:8081/index.ios.bundle?platform=ios"];
+    
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:@"RNHighScores"
+                                                 initialProperties:@{
+                                                                     @"scores": @[
+                                                                            @{
+                                                                             @"name": @"Alex",
+                                                                             @"value": @"42"
+                                                                             },
+                                                                            @{
+                                                                                @"name": @"Joel",
+                                                                                @"value": @"10",
+                                                                                }
+                                                                     ]
+                                                                     }
+                                                     launchOptions:nil];
+    
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+
+
+@end
